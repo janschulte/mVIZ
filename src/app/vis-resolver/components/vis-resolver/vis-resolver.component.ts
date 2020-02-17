a9a20f6bb6f67090b6111d2c29708ab946548fb2
Jan Schulte <j.schulte@52north.org>
2020-02-17 09:44:37 +0100

    improve vis table

--- src/app/vis-resolver/components/vis-resolver/vis-resolver.component.ts
+++ src/app/vis-resolver/components/vis-resolver/vis-resolver.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 
@@ -7,11 +8,20 @@ import { VisResolverService } from './../../vis-resolver.service';
 @Component({
   selector: 'app-vis-resolver',
   templateUrl: './vis-resolver.component.html',
-  styleUrls: ['./vis-resolver.component.scss']
+  styleUrls: ['./vis-resolver.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class VisResolverComponent implements OnInit {
 
   displayedColumns: string[] = ['label', 'score'];
+
+  expandedVis: Visualisation | null;
 
   public groups: CategoryGroup[] = [];
 
@@ -23,11 +33,14 @@ export class VisResolverComponent implements OnInit {
 
   ngOnInit() {
     this.groups = this.visRes.groups;
-    this.visRes.changedVisualizations.subscribe(vis => this.visualisations.data = this.prepareVisualizations(vis));
+    this.visRes.changedVisualizations.subscribe(vis => {
+      this.expandedVis = null;
+      this.prepareVisualizations(vis);
+    });
   }
 
-  private prepareVisualizations(vis: Visualisation[]): Visualisation[] {
-    return vis.filter(e => e.score > 0).sort((a, b) => b.score - a.score);
+  private prepareVisualizations(vis: Visualisation[]) {
+    this.visualisations.data = vis.filter(e => e.score > 0).sort((a, b) => b.score - a.score);
   }
 
 }
